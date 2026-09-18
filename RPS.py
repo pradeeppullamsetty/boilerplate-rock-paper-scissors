de34a66dcdac751d25cc694aadbedb5252c25076
pradeeppullamsetty <pradeeppullamsetty@gmail.com>
2026-09-18 06:37:12 +0000

    update

--- RPS.py
+++ RPS.py
@@ -1,10 +1,38 @@
-# The example function below keeps track of the opponent's history and plays whatever the opponent played two plays ago. It is not a very good player so you will need to change the code to pass the challenge.
+opponent_history = []
+my_history = []
+strategy_counter = {}
 
-def player(prev_play, opponent_history=[]):
-    opponent_history.append(prev_play)
+def player(prev_play):
+    global opponent_history, my_history, strategy_counter
 
-    guess = "R"
-    if len(opponent_history) > 2:
-        guess = opponent_history[-2]
+    if prev_play:
+        opponent_history.append(prev_play)
+    else:
+        opponent_history.clear()
+        my_history.clear()
+        strategy_counter.clear()
 
-    return guess
+    if len(opponent_history) < 3:
+        my_next_move = "R"
+        my_history.append(my_next_move)
+        return my_next_move
+
+    n = 3
+    recent_sequence = "".join(opponent_history[-n:])
+    
+    if len(opponent_history) > n:
+        prev_sequence = "".join(opponent_history[-(n + 1):-1])
+        last_move = opponent_history[-1]
+        key_str = prev_sequence + last_move
+        strategy_counter[key_str] = strategy_counter.get(key_str, 0) + 1
+
+    predicted_next_move = max(
+        ["R", "P", "S"],
+        key=lambda m: strategy_counter.get(recent_sequence + m, 0)
+    )
+
+    counters = {"R": "P", "P": "S", "S": "R"}
+    my_next_move = counters[predicted_next_move]
+
+    my_history.append(my_next_move)
+    return my_next_move
